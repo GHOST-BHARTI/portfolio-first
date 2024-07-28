0e8b99d85c753a05ba8f5391d512409aae35f9fe
Harsh Bharti <harshjnv2018@gmail.com>
2024-07-28 23:21:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+tl=gsap.timeline();
+tl.from("#heading",{
+    x:-150,
+    duration:0.7,
+    opacity:0,
+    delay:0.1,
+})
+tl.from("#name",{
+    x:-100,
+    duration:0.7,
+    opacity:0,
+    delay:-0.2,
+})
+tl.from("#intro-para h6",{
+    x:-100,
+    duration:0.7,
+    opacity:0,
+    delay:-0.2,
+})
+tl.from("#intro button",{
+    duration:0.7,
+    opacity:0,
+    delay:-0.3,
+})
