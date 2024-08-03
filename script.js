155c301adaa4c2877be4f0075b548416abd6c457
Harsh Bharti <harshjnv2018@gmail.com>
2024-08-03 14:16:51 +0530

    commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ tl.from("#heading",{
     x:-150,
     duration:0.7,
     opacity:0,
-    delay:0.1,
+    delay:0.6,
 })
 tl.from("#name",{
     x:-100,
@@ -21,4 +21,88 @@ tl.from("#intro button",{
     duration:0.7,
     opacity:0,
     delay:-0.3,
+})
+
+pl = gsap.timeline();
+// pl.from("#img1",{
+//     x:-200,
+//     duration:1,
+//     opacity:0
+// },'a')
+// pl.from("#img2",{
+//     x:200,
+//     duration:1,
+//     opacity:0
+// },'a')
+// pl.from("#right h1",{
+//     x:100,
+//     duration:1,
+//     opacity:0
+// },'a')
+// pl.from("#left h1",{
+//     x:-100,
+//     duration:1,
+//     opacity:0
+// },'a')
+// pl.from("#right h5",{
+//     x:100,
+//     duration:1,
+//     opacity:0
+// },'a')
+// pl.from("#left h5",{
+//     x:-100,
+//     duration:1,
+//     opacity:0
+// },'a')
+gsap.from("#img1",{
+    scale:0,
+    x:-200,
+    opacity:0,
+    scrollTrigger:{
+        trigger:"#right-top",
+        scroller:"body",
+        // markers:true,
+        start:"top 100%",
+        end:"top 40%",
+        scrub:2,
+    }
+})
+gsap.from("#img2",{
+    scale:0,
+    x:200,
+    opacity:0,
+    scrollTrigger:{
+        trigger:"#right-top",
+        scroller:"body",
+        // markers:true,
+        start:"top 100%",
+        end:"top 40%",
+        scrub:2,
+    }
+})
+gsap.from("#img3",{
+    scale:0,
+    x:-200,
+    opacity:0,
+    scrollTrigger:{
+        trigger:"#right-bottom",
+        scroller:"body",
+        // markers:true,
+        start:"-20% 100%",
+        end:"-20% 40%",
+        scrub:2,
+    }
+})
+gsap.from("#img4",{
+    scale:0,
+    x:200,
+    opacity:0,
+    scrollTrigger:{
+        trigger:"#right-bottom",
+        scroller:"body",
+        // markers:true,
+        start:"-20% 100%",
+        end:"-20% 40%",
+        scrub:2,
+    }
 })
